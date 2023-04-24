4930a61bd85f7df98e538f0774beac1b75341a97
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-24 14:13:18 +0500

    Update uname.c

--- src/fake-environ/uname.c
+++ src/fake-environ/uname.c
@@ -1,10 +1,8 @@
-
-
 #include <stdio.h>
 #include <string.h>
 #include <dlfcn.h>
-#include <stdlib.h>		/* for EXIT_FAILURE */
-#include <unistd.h>		/* for _exit() */
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -12,39 +10,40 @@
 #include <sys/utsname.h>
 
 #ifndef RTLD_NEXT
-#define RTLD_NEXT      ((void *) -1l)
+#define RTLD_NEXT ((void *)-1l)
 #endif
 
-typedef int (*uname_t)(struct utsname * buf);
+typedef int (*uname_t)(struct utsname *);
 
 static void *get_libc_func(const char *funcname) {
-	char *error;
-
-	void *func = dlsym(RTLD_NEXT, funcname);
-	if ((error = dlerror()) != NULL) {
-		fprintf(stderr, "I can't locate libc function `%s' error: %s", funcname, error);
-		_exit(EXIT_FAILURE);
-	}
-
-	return func;
+    void *func = dlsym(RTLD_NEXT, funcname);
+    if (!func) {
+        syslog(LOG_ERR, "Failed to locate libc function %s: %s", funcname, dlerror());
+        _exit(EXIT_FAILURE);
+    }
+    return func;
 }
 
 int uname(struct utsname *buf) {
-	char *env = NULL;
+    char *env = NULL;
 
-	/* Call real uname to get the information we need. */
-	uname_t real_uname = (uname_t)get_libc_func("uname");
-	int ret = real_uname((struct utsname *) buf);
+    uname_t real_uname = get_libc_func("uname");
+    int ret = real_uname(buf);
 
-	/* Replace release if requested. */
-	if ((env = getenv("UTS_RELEASE")) != NULL) {
-		strncpy(buf->release, env, _UTSNAME_RELEASE_LENGTH);
-	}
+    if (ret < 0) {
+        syslog(LOG_ERR, "Failed to get system information: %m");
+        return ret;
+    }
 
-	/* Replace machine type if requested. */
-	if ((env = getenv("UTS_MACHINE")) != NULL) {
-		strncpy(buf->machine, env, _UTSNAME_MACHINE_LENGTH);
-	}
+    /* Replace release if requested. */
+    if ((env = getenv("UTS_RELEASE")) != NULL) {
+        strncpy(buf->release, env, _UTSNAME_RELEASE_LENGTH);
+    }
 
-	return ret;
+    /* Replace machine type if requested. */
+    if ((env = getenv("UTS_MACHINE")) != NULL) {
+        strncpy(buf->machine, env, _UTSNAME_MACHINE_LENGTH);
+    }
+
+    return ret;
 }
